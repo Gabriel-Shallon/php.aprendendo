01c636372a5416534c85604f2505501a2c6e1634
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-29 17:19:16 -0400

    Update app.php

--- app.php
+++ app.php
@@ -4,7 +4,7 @@
     $FilhoDaPessoa1 = new filhodapessoa("Neto", 36, "neto@neto.pro.br");
     echo($FilhoDaPessoa1->getnome());
 
-    $pessoa1 = new Pessoa();
-    $pessoa1->cadastrar("Neto", 36)
-    echo (pessoa1->getIdade());
+    $pessoa1 = new Pessoa("Neto", 40);
+    $pessoa1->cadastrar("Neto", 2)
+    
 ?>
